efbb1c6fb334160ec5764ce3ee0236883a29ee1c
victor.orellana <victor.orellana@gmail.com>
2025-05-16 14:07:45 -0300

    feat: add screens for displaying anime details and favorites

--- app/src/main/java/br/com/mendo/myanimelist/MainActivity.kt
+++ app/src/main/java/br/com/mendo/myanimelist/MainActivity.kt
@@ -3,23 +3,45 @@ package br.com.mendo.myanimelist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import br.com.mendo.myanimelist.ui.theme.MyanimelistTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.mendo.myanimelist.data.model.Anime
+import br.com.mendo.myanimelist.presentation.AnimeDetailsScreen
+import br.com.mendo.myanimelist.presentation.FavoritesScreen
+import br.com.mendo.myanimelist.presentation.PopularAnimesScreen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyanimelistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                }
+            MaterialTheme {
+                AnimeAppNavigation()
             }
+        }
+    }
+}
+
+@Composable
+fun AnimeAppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "popular") {
+        composable("popular") {
+            PopularAnimesScreen { anime ->
+                navController.navigate("details/${anime.malId}")
+            }
+        }
+        composable("favorites") {
+            FavoritesScreen()
+        }
+        composable("details/{animeId}") { backStackEntry ->
+            val animeId = backStackEntry.arguments?.getString("animeId")?.toIntOrNull()
+            val anime = navController.previousBackStackEntry?.savedStateHandle?.get<Anime>("anime")
+            anime?.let { AnimeDetailsScreen(it) }
         }
     }
 }
